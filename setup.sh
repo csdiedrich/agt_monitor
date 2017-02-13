38a3b05dcbde743e9606a3edd4b14c0c71ab3d3c
csdiedrich <csdiedrich@gmail.com>
2017-02-13 18:01:02 -0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 ssh-keygen -f $HOME/.ssh/id_rsa -t rsa -N ''
-/root/scripts/login.sh
+/usr/bin/expect -c 'spawn ssh-copy-id root@nagios
+expect "password:"
+send "'$PWD_ROOT'\n"
+expect eof'
 ip=$(ip addr |grep eth0|grep -v UP|awk -F"/" '{print $1}'|awk '{print $2}')
 host=$(hostname)
 sed -i 's/IP/$ip/g' /opt/template.cfg
