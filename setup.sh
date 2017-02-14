612e33a0f2680811fe3f9c2ae9e44479a03727f3
csdiedrich <csdiedrich@gmail.com>
2017-02-14 09:58:30 -0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,6 +8,6 @@ send "'$PWD_ROOT'\n"
 expect eof'
 ip=$(ip addr |grep eth0|grep -v UP|awk -F"/" '{print $1}'|awk '{print $2}')
 host=$(hostname)
-sed -i 's/IP/$ip/g' /opt/template.cfg
-sed -i 's/HOSTNAME/$host/g' /opt/template.cfg
+sed -i "s/IP/$ip/g" /opt/template.cfg
+sed -i "s/HOSTNAME/$host/g" /opt/template.cfg
 scp /opt/template.cfg root@nagios:/opt/nagios/etc/monitor/$host.cfg
