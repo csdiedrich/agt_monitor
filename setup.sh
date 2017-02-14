f285741f0228bd965c2436f66a2170cc8e4276b9
csdiedrich <csdiedrich@gmail.com>
2017-02-14 12:32:09 -0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ send "'$PWD_ROOT'\n"
 expect eof'
 ip=$(ip addr |grep eth0|grep -v UP|awk -F"/" '{print $1}'|awk '{print $2}'|head -1)
 host=$(hostname)
-$ip_nagios=$(ping -c1 nagios|head -1|awk '{print $3}'|sed 's/(//g'|sed 's/)//g')
+ip_nagios=$(ping -c1 nagios|head -1|awk '{print $3}'|sed 's/(//g'|sed 's/)//g')
 sed -i "s/IP/$ip/g" /opt/template.cfg
 sed -i "s/HOSTNAME/$host/g" /opt/template.cfg
 sed -i "s/IP_NAGIOS/$ip_nagios/g" /etc/nagios/nrpe.cfg
