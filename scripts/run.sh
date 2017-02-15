e46eaeac04e746a77f5ed1a40ac3d7d9c8610d42
csdiedrich <csdiedrich@gmail.com>
2017-02-15 11:43:30 -0200

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 /root/scripts/setup.sh
 /etc/init.d/nagios-nrpe-server start
+ssh root@nagios rm -f /opt/nagios/etc/objects/localhost.cfg
 ssh root@nagios /root/scripts/reload.sh
 ping 8.8.8.8 > /dev/null
