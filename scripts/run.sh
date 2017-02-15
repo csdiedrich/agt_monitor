a89704035918509a1c2f239a671724b2462aedac
csdiedrich <csdiedrich@gmail.com>
2017-02-15 13:29:57 -0200

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,5 +2,6 @@
 /root/scripts/setup.sh
 /etc/init.d/nagios-nrpe-server start
 ssh root@nagios rm -f /opt/nagios/etc/objects/localhost.cfg
+ssh root@nagios sed -i 's/cfg_file=\/opt\/nagios\/etc\/objects\/localhost.cfg/#cfg_file=\/opt\/nagios\/etc\/objects\/localhost.cfg/g' /opt/nagios/etc/nagios.cfg
 ssh root@nagios /root/scripts/reload.sh
 ping 8.8.8.8 > /dev/null
