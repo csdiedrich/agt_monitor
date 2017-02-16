98b9a0bf099fa7ddba1a886568e43af40b7e213f
csdiedrich <csdiedrich@gmail.com>
2017-02-16 15:06:54 -0200

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,8 +9,18 @@ expect eof'
 ip=$(ip addr |grep eth0|grep -v UP|awk -F"/" '{print $1}'|awk '{print $2}'|head -1)
 host=$(cat /etc/hostname)
 ip_nagios=$(ping -c1 nagios|head -1|awk '{print $3}'|sed 's/(//g'|sed 's/)//g')
-sed -i "s/IP/$ip/g" /opt/template.cfg
-sed -i "s/HOSTNAME/$host/g" /opt/template.cfg
-sed -i "s/IP_NAGIOS/$ip_nagios/g" /etc/nagios/nrpe.cfg
-scp /opt/template.cfg root@nagios:/opt/nagios/etc/monitor/$host.cfg
+python /usr/lib/nagios/plugins/check_swarm managers > /dev/null 2>&1
+if [ $? = 0 ]; then
+  sed -i "s/IP/$ip/g" /opt/template_manager.cfg
+  sed -i "s/HOSTNAME/$host/g" /opt/template_manager.cfg
+  sed -i "s/IP_NAGIOS/$ip_nagios/g" /etc/nagios/nrpe.cfg
+  sed -i "s/SERVICE/$SERVICE/g" /opt/template_manager.cfg
+  scp /opt/template_manager.cfg root@nagios:/opt/nagios/etc/monitor/$host.cfg
+else
+  sed -i "s/IP/$ip/g" /opt/template_node.cfg
+  sed -i "s/HOSTNAME/$host/g" /opt/template_node.cfg
+  sed -i "s/IP_NAGIOS/$ip_nagios/g" /etc/nagios/nrpe.cfg
+  sed -i "s/SERVICE/$SERVICE/g" /opt/template_node.cfg
+  scp /opt/template_node.cfg root@nagios:/opt/nagios/etc/monitor/$host.cfg
+fi
 usermod nagios -s /bin/bash
